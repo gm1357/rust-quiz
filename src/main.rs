c3bd08bc8665d53cd00c88e6f21ace8a4bef8297
Gabriel Machado <g_machado1@hotmail.com>
2021-02-16 14:24:15 -0300

    Added dependencies and basics to make request and get response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use serde::Deserialize;
+use reqwest::Error;
+
+#[derive(Deserialize, Debug)]
+struct Quiz {
+    response_code: u32,
+    results: Vec<Question>
+}
+
+#[derive(Deserialize, Debug)]
+struct Question {
+    category: String,
+    question: String,
+    difficulty: String,
+    correct_answer: String,
+    incorrect_answers: Vec<String>
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let request_url = format!(
+        "https://opentdb.com/api.php?amount={amount}&category={category}&type=multiple",
+        amount = "10",
+        category = "0"
+    );
+    println!("{}", request_url);
+    let response = reqwest::get(&request_url).await?;
+
+    let quiz_questions = response.json::<Quiz>().await?;
+    println!("{:?}", quiz_questions);
+    Ok(())
 }
