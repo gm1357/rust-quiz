0b7856ef97d19bb8a468b55ea836a6fe16ec0175
Gabriel Machado <g_machado1@hotmail.com>
2021-02-16 14:52:38 -0300

    Added reading command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use serde::Deserialize;
 use reqwest::Error;
+use std::env;
+use std::process;
 
 #[derive(Deserialize, Debug)]
 struct Quiz {
@@ -16,14 +18,37 @@ struct Question {
     incorrect_answers: Vec<String>
 }
 
+struct Config {
+    amount: String,
+    category: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("please provide amount of questions and category type.");
+        }
+
+        let amount = args[1].clone();
+        let category = args[2].clone();
+
+        Ok(Config { amount, category })
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
     let request_url = format!(
         "https://opentdb.com/api.php?amount={amount}&category={category}&type=multiple",
-        amount = "10",
-        category = "0"
+        amount = config.amount,
+        category = config.category
     );
-    println!("{}", request_url);
     let response = reqwest::get(&request_url).await?;
 
     let quiz_questions = response.json::<Quiz>().await?;
